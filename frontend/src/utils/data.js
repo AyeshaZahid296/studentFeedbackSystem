6be68b29d07bad89a96107016faee698be5e6e5f
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-07-27 18:16:37 +0500

    data & halper.js done

---
+++ frontend/src/utils/data.js
@@ -0,0 +1,17 @@
+import { LuLayoutDashboard, LuLogOut } from "react-icons/lu";
+
+export const SIDE_MENU_DATA = [
+    {
+        id: "01",
+        label: "Dashboard",
+        icon: LuLayoutDashboard,
+        path: "/admin/dashboard",
+    },
+    {
+        id: "02",
+        label: "Logout",
+        icon: LuLogOut,
+        path: "logout",
+    },
+];
+
