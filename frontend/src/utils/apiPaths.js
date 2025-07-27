87d87a74eba907293cf5614f4b068d0213b84cb9
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-07-27 23:48:12 +0500

    vercel.json

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,14 +1,14 @@
 // Base URL — will be used in axiosInstance.js
-export const BASE_URL = "http://localhost:8000/api"; // Make sure your backend uses /api prefix
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL;; // Make sure your backend uses /api prefix
 
 //  API Endpoints
 export const API_PATHS = {
     FEEDBACK: {
-        SUBMIT: "/feedback",           // POST – student feedback form
-        GET_ALL: "/feedback",         // GET – all feedbacks (admin only)
+        SUBMIT: "/api/feedback",           // POST – student feedback form
+        GET_ALL: "/api/feedback",         // GET – all feedbacks (admin only)
         DELETE: (id) => `/feedback/${id}`, // DELETE – delete by ID
     },
     ADMIN: {
-        LOGIN: "/auth/login",         // POST – admin login
+        LOGIN: "/api/auth/login",         // POST – admin login
     },
 };
